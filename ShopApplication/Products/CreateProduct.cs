c8ef52572a89984084570c1e97a9da8fc17cfe00
Jason Qu <fishinsea2014@gmail.com>
2019-07-09 01:03:02 +1200

    Setup Project

---
+++ ShopApplication/Products/CreateProduct.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Shop.Database;
+using ShopDomain.Models;
+
+namespace Shop.Application.Products
+{
+    class CreateProduct
+    {
+        private ApplicationDbContext _context;
+        public CreateProduct(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Do(int id, string Name, string Description )
+        {
+            _context.Products.Add(new Product
+            {
+                Id = id,
+                Name = Name,
+                Description = Description            
+            });
+
+        }
+    }
+}
