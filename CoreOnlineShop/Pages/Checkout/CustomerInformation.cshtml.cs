5f84705c1c4a1ef2816bed9765180256c0bf87e7
Jason Qu <fishinsea2014@gmail.com>
2019-07-19 14:56:38 +1200

    Complete customer info management.

---
+++ CoreOnlineShop/Pages/Checkout/CustomerInformation.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Shop.Application.Cart;
+using Shop.Database;
+
+namespace CoreOnlineShop.Pages.Checkout
+{
+    public class CustomerInformationModel : PageModel
+    {
+        [BindProperty]
+        public AddCustomerInformation.Request CustomerInformation { get; set; }
+
+        public IActionResult OnGet()
+        {
+            //Get Cart
+            //If cart exists, then go to payment
+            var information = new GetCustomerInformation(HttpContext.Session).Do();
+            if (information == null)
+            {
+                return Page();
+            }
+            else
+            {
+                return RedirectToPage("/Checkout/Payment");
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            //Post cart
+            new AddCustomerInformation(HttpContext.Session).Do(CustomerInformation);
+            return RedirectToPage("Payment");
+        }
+    }
+}
--- ShopDomain/Models/Order.cs
+++ ShopDomain/Models/Order.cs
@@ -7,11 +7,19 @@ namespace ShopDomain.Models
     public class Order
     {
         public int Id { get; set; }
-        public string OrderRef { get; set; }    
+        public string OrderRef { get; set; }
+
+
+        #region Customer Information
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
         public string Address1 { get; set; }
         public string Address2 { get; set; }
         public string City { get; set; }
-        public string PostCode { get; set; }
+        public string PostCode { get; set; } 
+        #endregion
 
         public ICollection<OrderProduct> OrderProducts { get; set; }
     }
