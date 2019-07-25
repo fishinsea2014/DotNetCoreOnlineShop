8054203eddfe2ebfb52cd5a9318fdf9096fc010f
Jason Qu <fishinsea2014@gmail.com>
2019-07-26 01:31:43 +1200

    Complete out of stock options.

--- ShopApplication/Products/GetProduct.cs
+++ ShopApplication/Products/GetProduct.cs
@@ -43,7 +43,7 @@ namespace Shop.Application.Products
                         {
                             Id = y.Id,
                             Description=y.Description,
-                            InStock = y.Qty > 0
+                            Qty = y.Qty 
                         })
                     })
                     .FirstOrDefault();
@@ -61,7 +61,7 @@ namespace Shop.Application.Products
         {
             public int Id { get; set; }
             public string Description { get; set; }
-            public bool InStock { get; set; }
+            public int Qty { get; set; }
         }
     }
 }
